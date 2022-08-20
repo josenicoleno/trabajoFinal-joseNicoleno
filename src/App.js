4cd61e5fe799468b0c2ec44179f832e001cc8b88
Jose Nicoleno <josenicoleno@hotmail.com>
2022-08-20 16:41:13 -0300

    trabajo final

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+  /* const [carrito, setCarrito] = useState([])
+  useEffect(() => {
+    getCart()
+      .then((snp) => {
+        setCarrito(
+          snp.docs.map((document) => ({
+            ...document.data(),
+            id: document.id
+          }))
+        )
+      })
+  }, [])
+ */
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    {/* < CartContextProvider products={carrito}>
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path='/' element={<App />} />
+          <Route index element={<ItemListContainer />} />
+          <Route path='personajes/:id' element={<ItemDetailContainer />} />
+          <Route path='episodios' element={<Episodies />} />
+          <Route path='episodio/:id' element={<EpisodeDetailContainer />} />
+          <Route path='*' element={<PageNotFound />} />
+          <Route path='carrito' element={<Cart />} />
+          <Route path='sobre-nostros' element={<SobreNosotros />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </CartContextProvider > */}
   );
 }
 
